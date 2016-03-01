5ecd36726205b68ca3ef5f9b40256fc380a240bb
José Carlos Martins <naoedojose.nave@gmail.com>
2016-03-01 11:58:43 -0300

    Multiplayer Start !

---
+++ Assets/_Script/SpawnPosition.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnPosition : MonoBehaviour
+{
+
+
+    public Transform[] spawnPos;
+
+    // Use this for initialization
+    void Start()
+    {
+        spawnPos = GetComponentsInChildren<Transform>();
+    }
+
+}
