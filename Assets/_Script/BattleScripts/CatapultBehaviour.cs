cd2060f8ac044c6a1bb1aa7a4bdc1e58720d7993
Thiago Torres <TheThiagoo0@gmail.com>
2016-03-08 11:51:22 -0300

    CatapultBehaviour
    Script criado para o comportamento de estilingue. Funções que entram em
    modo estilingue criadas. Cena de desenvolvimento da mesma criada.

---
+++ Assets/_Script/BattleScripts/CatapultBehaviour.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CatapultBehaviour : MonoBehaviour {
+	
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (isPressed () == true) {
+			changeMov (false);
+		} else {
+			changeMov (true);
+		}
+    } 
+
+	private void changeMov(bool allow){
+        if (allow == true) {
+            gameObject.GetComponent<Player>().enabled = true;
+        } else {
+            gameObject.GetComponent<Player>().enabled = false;
+        }
+	}
+
+	private bool isPressed(){
+		if (Input.GetAxis ("CatapultTest") < 0 ) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
