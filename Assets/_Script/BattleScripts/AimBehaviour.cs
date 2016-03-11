966686c38b9c78f4065d264a5b473a1fc7721a52
Thiago Torres <TheThiagoo0@gmail.com>
2016-03-11 10:34:36 -0300

    Aim update
    Script ok ok

--- Assets/_Script/BattleScripts/AimBehaviour.cs
+++ Assets/_Script/BattleScripts/AimBehaviour.cs
@@ -12,6 +12,22 @@ public class AimBehaviour : MonoBehaviour {
         position_x = Input.GetAxis("Horizontal");
         position_y = Input.GetAxis("Vertical");
 
+        if (position_x != 0 || position_y != 0) {
+            transform.position = new Vector3(transform.position.x + position_x, transform.position.y + position_y, 0);
+            adjustLimits();
+        } else {
+            adjustToCenter();
+        }
+    }
+
+    void adjustToCenter() {
+        print("Hey, need to adjust to center!");
+        Vector3 parentPosition = transform.parent.position;
         transform.position = new Vector3(transform.position.x + position_x, transform.position.y + position_y, 0);
+    }
+
+    void adjustLimits() {
+        print("Hey, need to adjust the limits!");
+        Vector3 parentPosition = transform.parent.position;
     }
 }
