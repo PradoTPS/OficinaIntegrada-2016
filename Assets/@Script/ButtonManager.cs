a53a31a80e83985962ddc198ef4a23f87f5f44b4
Thiago do Prado Silva <thiagosemjoelho@gmail.com>
2016-07-13 22:11:27 -0300

    Scenes. Some scenes
    -Start making a scene manager;

---
+++ Assets/@Script/ButtonManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class ButtonManager : MonoBehaviour {
+
+	public string nextScene;
+
+	public void GoTo(){
+		SceneManager.LoadScene (nextScene);
+	}
+}
