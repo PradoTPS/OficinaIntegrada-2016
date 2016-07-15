5f2ef1e85f2630561e59654f07e5d9d5d9c3c314
Thiago do Prado Silva <thiagosemjoelho@gmail.com>
2016-07-14 22:21:17 -0300

    Navigation and Video
    -Scenes navigation implemented;
    -Video added;

---
+++ Assets/@Script/KeepVideo.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEditor.SceneManagement;
+using System.Collections;
+
+public class KeepVideo : MonoBehaviour {
+
+	void Start(){
+		KeepOne ();
+	}
+
+	void Update(){
+		KeepIt ();
+	}
+
+	void KeepIt(){
+		if (EditorSceneManager.GetActiveScene ().name == "Game") {
+			Destroy (gameObject);
+		} else {
+			DontDestroyOnLoad (gameObject);
+		}
+	}
+
+	void KeepOne(){
+		if (GameObject.FindGameObjectsWithTag ("Video").Length > 1) {
+			Destroy (gameObject);
+		}
+	}
+}
---
+++ Assets/@Script/PlayVideo.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayVideo : MonoBehaviour {
+	
+	public MovieTexture video;
+
+	void Awake() {
+		GetComponent<Renderer>().material.mainTexture = video;
+		video.loop = true;
+	}
+
+	void Update(){
+		Play ();
+	}
+
+	void Play(){
+		if (!video.isPlaying) {
+			video.Play ();
+		}
+	}
+}
