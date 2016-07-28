8be55590b0237cdee7cc6d9a66b2bf9a4764aeab
Thiago Torres <TheThiagoo0@gmail.com>
2016-07-28 12:26:57 -0300

    Starting counting
    -Some adjustes;

---
+++ Assets/@Script/CountingToGame.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CountingToGame : MonoBehaviour {
+	#region Properties
+	private GameObject[] selectables = new GameObject[4];
+	#endregion
+
+	#region Methods
+	void Awake(){
+		for (int i = 0; i < selectables.Length; i++) {
+			selectables [i] = GameObject.Find("Selectable" + (i + 1).ToString());
+		}
+	}
+	void Counting(){
+
+	}
+	#endregion
+}
