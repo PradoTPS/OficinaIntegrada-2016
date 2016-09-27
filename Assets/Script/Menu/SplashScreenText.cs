b2cc85e6d5a23c903b7c11b2e73024edf9429628
FilipeMFernandes <filipemateusreis@gmail.com>
2016-09-27 12:14:04 -0300

    Splash Screen Text
    Splash Screen Text change depending on the imput the player are using

---
+++ Assets/Script/Menu/SplashScreenText.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
+using XboxCtrlrInput;
+using KeyboardInput;
+
+public class SplashScreenText : MonoBehaviour {
+
+	int queriedNumberOfCtrlrs = XCI.GetNumPluggedCtrlrs ();
+	public Text TriggerTxt;
+	// Use this for initialization
+	void Start () {
+		if (queriedNumberOfCtrlrs == 1){
+			TriggerTxt.text = "sgasdg";
+		}else{
+			TriggerTxt.text = "Press SPACE to continue";
+		}
+	}
+}
