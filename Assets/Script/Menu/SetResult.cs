193bb328c8a056fd81920ff2dbf52a463bc8db44
ThiagoZx <TheThiagoo0@gmail.com>
2016-11-25 03:28:42 -0200

    :taxi:
    ...

--- Assets/Script/Menu/SetResult.cs
+++ Assets/Script/Menu/SetResult.cs
@@ -13,9 +13,8 @@ public class SetResult : MonoBehaviour {
 	public GameObject particle;
 	public GameObject[] explosions;
 
-	public Image winnerImage;
-	public Sprite drawImage;
 	public Text winnerText;
+	private List<Vector2> order = new List<Vector2>();
 
 	private string winnerName;
 	private bool isSet;
@@ -25,10 +24,13 @@ public class SetResult : MonoBehaviour {
 
 	#region Methods
 	void Start () {
-		container.transform.position = new Vector2 ((400 - (PlayerPrefs.GetInt ("numPlayers") * 100)) / 45, 0);	
+		//I mean, if you don't divide this by 45, makes perfect, PERFECT sense, but it returns 6524... Welp.
+		float number = (280 - (70 * PlayerPrefs.GetInt ("numPlayers"))) / 45;
+		container.transform.position = new Vector2 (number, container.transform.position.y);	
 		GameObject.Find ("AudioHandler").GetComponent<AudioBehaviour> ().audios [15].Play ();
 		StartCoroutine (changeBoolInSec (isSet, 5.65f));
 		isSet = false;
+		SettingPositions ();
 		SettingPlayers ();
 	}
 
@@ -41,34 +43,112 @@ public class SetResult : MonoBehaviour {
 		}
 	}
 
+	void SettingPositions(){
+		for (int i = 0; i < 4; i++) {
+			Vector2 temp = new Vector2 (PlayerPrefs.GetInt ("RoundWinner " + (i + 1)), 1);
+			order.Add (temp);
+		}
+
+		for (int i = 0; i < PlayerPrefs.GetInt ("numPlayers"); i++) {
+			for (int j = 0; j < PlayerPrefs.GetInt ("numPlayers"); j++) {
+				if (order [i].x < order [j].x) {
+					Vector2 temp = new Vector2 (order [i].x, order [i].y + 1);
+					order [i] = temp;
+				}
+			}
+			print (order [i]);
+		}
+	}
+
 	void SettingPlayers() {
 		for (int i = 0; i < PlayerPrefs.GetInt ("numPlayers"); i++) {
-			players[i] = GameObject.Find("Player" + ( i + 1 ).ToString() + " " + "HUD");
+
+			players[i] = GameObject.Find("Player" + (order[i].y).ToString() + " " + "HUD");
+
 			players [i].name = players [i].GetComponent<Image> ().sprite.name;
 			players [i].GetComponentInChildren<SpriteRenderer> ().sprite = players [i].GetComponent<Image> ().sprite;
 			players [i].GetComponent<Image> ().sprite = GetComponent<SetHud> ().HUDImages [11];
-			Instantiate(particle, new Vector2(players [i].transform.position.x, players [i].transform.position.y), Quaternion.Euler(0, 0, 0));
+
+			//The particle on them
+			InstantiateParticle(players [i].transform.position.x, players [i].transform.position.y, players [i].name);
 		}
+	}
+
+	void InstantiateParticle(float x, float y, string name){
+
+		GameObject temp;
+		Color color;
+
+		switch (name) {
+		case "Fred":
+			color = new Color (254, 139, 174);
+			temp = Instantiate (particle, new Vector2 (x, y), Quaternion.Euler (0, 0, 0)) as GameObject;
+			temp.GetComponent<ParticleSystem> ().startColor = color;
+			break;
+		case "Ingreed":
+			color = new Color (136, 126, 168);
+			temp = Instantiate (particle, new Vector2 (x, y), Quaternion.Euler (0, 0, 0)) as GameObject;
+			temp.GetComponent<ParticleSystem> ().startColor = color;
+			break;
+		case "Purpled":
+			color = new Color (252, 110, 111);
+			temp = Instantiate(particle, new Vector2(x, y), Quaternion.Euler(0, 0, 0)) as GameObject;
+			temp.GetComponent<ParticleSystem> ().startColor = color;
+			break;
+		case "Red":
+			color = new Color (131, 89, 152);
+			temp = Instantiate(particle, new Vector2(x, y), Quaternion.Euler(0, 0, 0)) as GameObject;
+			temp.GetComponent<ParticleSystem> ().startColor = color;
+			break;
+		case "Schnapps":
+			color = new Color (216, 218, 218);
+			temp = Instantiate(particle, new Vector2(x, y), Quaternion.Euler(0, 0, 0)) as GameObject;
+			temp.GetComponent<ParticleSystem> ().startColor = color;
+			break;
+		case "Seaweed":
+			color = new Color (69, 182, 187);
+			temp = Instantiate(particle, new Vector2(x, y), Quaternion.Euler(0, 0, 0)) as GameObject;
+			temp.GetComponent<ParticleSystem> ().startColor = color;
+			break;
+		case "Speed":
+			color = new Color (255, 123, 72);
+			temp = Instantiate(particle, new Vector2(x, y), Quaternion.Euler(0, 0, 0)) as GameObject;
+			temp.GetComponent<ParticleSystem> ().startColor = color;
+			break;
+		case "Sr.Ed":
+			color = new Color (59, 56, 56);
+			temp = Instantiate(particle, new Vector2(x, y), Quaternion.Euler(0, 0, 0)) as GameObject;
+			temp.GetComponent<ParticleSystem> ().startColor = color;
+			break;
+		case "Tedd":
+			color = new Color (234f, 167, 71);
+			temp = Instantiate(particle, new Vector2(x, y), Quaternion.Euler(0, 0, 0)) as GameObject;
+			temp.GetComponent<ParticleSystem> ().startColor = color;
+			break;
+		case "Weed":
+			color = new Color (152, 195, 80);
+			temp = Instantiate (particle, new Vector2 (x, y), Quaternion.Euler (0, 0, 0)) as GameObject;
+			temp.GetComponent<ParticleSystem> ().startColor = new Color (152, 195, 80, 1f);
+			break;
+		}	
 	}
 
 	void SettingWinner() {
 		int highestScore = 0;
+
 		for (int i = 0; i < PlayerPrefs.GetInt ("numPlayers"); i++) {
 			if (highestScore == 0) {
 				highestScore = int.Parse (Score [i].GetComponent<Text> ().text);
 				winnerName = players [i].name;
-				winnerImage.sprite = players [i].GetComponentInChildren<SpriteRenderer> ().sprite;
-				winnerText.text = winnerName + " is the Winner";
+				winnerText.text = winnerName + " Won!";
 			} else if (highestScore < int.Parse (Score [i].GetComponent<Text> ().text)) {
 				highestScore = int.Parse (Score [i].GetComponent<Text> ().text);
 				winnerName = players [i].name;
-				winnerImage.sprite = players [i].GetComponentInChildren<SpriteRenderer> ().sprite;
-				winnerText.text = winnerName + " is the Winner";
+				winnerText.text = winnerName + " Won!";
 			} else if (highestScore == int.Parse (Score [i].GetComponent<Text> ().text)) {
 				highestScore = int.Parse (Score [i].GetComponent<Text> ().text);
 				winnerName = players [i].name;
-				winnerImage.sprite = drawImage;
-				winnerText.text = "Ohhh! It's a draw!";
+				winnerText.text = "It's a draw!";
 			}
 		}
 	}
@@ -89,8 +169,6 @@ public class SetResult : MonoBehaviour {
 		Color color = new Color();	
 
 		for(int i = 0; i < 4; i++){
-			//I hate myself for this.
-
 			switch (winnerName) {
 				case "Fred":
 					color = new Color (254, 139, 174);
@@ -131,8 +209,6 @@ public class SetResult : MonoBehaviour {
 
 	void FixPlayers () {
 		GameObject[] particles = GameObject.FindGameObjectsWithTag ("Particle");
-
-
 		for (int i = 0; i < PlayerPrefs.GetInt ("numPlayers"); i++) {
 			players [i].GetComponent<Image> ().sprite = players [i].GetComponentInChildren<SpriteRenderer> ().sprite;
 			Destroy (particles [i]);
